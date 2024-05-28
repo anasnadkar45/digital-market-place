8c4f124b18aa31ddb550543105079b46f9bad6a3
anasnadkar45 <anasnadkar23@gmail.com>
2024-05-28 20:48:40 +0530

    updating codebase

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,10 +1,11 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SelectCategory } from "../components/SelectCategory";
 import { Textarea } from "@/components/ui/textarea";
 import { TipTapEditor } from "../components/Editor";
 import { UploadDropzone } from "../lib/uploadthing";
+import { Button } from "@/components/ui/button";
 
 export default function SellRoute() {
     return (
@@ -51,7 +52,15 @@ export default function SellRoute() {
                             <Label>Product Images</Label>
                             <UploadDropzone endpoint="imageUploader" className="border border-white"/>
                         </div>
+
+                        <div className="flex flex-col gap-y-2">
+                            <Label>Product File</Label>
+                            <UploadDropzone endpoint="productFileUpload" className="border border-white"/>
+                        </div>
                     </CardContent>
+                    <CardFooter className="mt-5">
+                        <Button>Submit form</Button>
+                    </CardFooter>
                 </form>
             </Card>
         </section>
